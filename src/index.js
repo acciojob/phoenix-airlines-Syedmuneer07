124a62da0ac54ad3f5fd641cd60214cd60dd2781
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2026-01-23 01:57:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
+const root=REACTDOM.createRoot(document.getElementById("root"));
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <app/>
+  </Provider>
+);
