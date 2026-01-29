47fe868daef4cd88f51393c788c5ad0c7f602065
syemie069 <syemie069@gmail.com>
2026-01-29 21:37:08 +0530

    2nd  commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 
-const root=REACTDOM.createRoot(document.getElementById("root"));
-
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-    <app/>
-  </Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
